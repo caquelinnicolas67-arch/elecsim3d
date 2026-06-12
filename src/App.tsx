a8a83bddce5d57e8d9175c0cfe83a9269d50e204
caquelinnicolas67-arch <caquelin.nicolas67@gmail.com>
2026-06-12 21:07:36 +0200

    Correction affichage

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 export default function App() {
   return (
-    <div style={{ 
-      width: "100vw", 
-      height: "100vh",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      background: "#111",
-      color: "white",
-      fontSize: "24px"
-    }}>
+    <div
+      style={{
+        width: "100%",
+        height: "100vh",
+        margin: 0,
+        overflow: "hidden",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        background: "#111",
+        color: "white",
+        fontSize: "24px"
+      }}
+    >
       ⚡ ElecSim3D est en ligne ⚡
     </div>
   );
